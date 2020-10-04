7fe82c7bfc3cdf2c1854b79329da6eb1f37289f0
miaotony <41962043+miaotony@users.noreply.github.com>
2020-10-04 21:40:42 +0800

    [feat] Add crawler with multiprocessing. 并行爬取数据，优化重试逻辑。

--- crawler.py
+++ crawler.py
@@ -8,6 +8,7 @@ NUAA Library Flow Crawler
 
 @Author: MiaoTony
 @CreateTime: 20200918
+@UpdateTime: 20201004
 """
 
 import requests
@@ -17,6 +18,7 @@ import json
 import time
 import random
 import datetime
+import multiprocessing
 
 
 class Flow(object):
@@ -65,14 +67,20 @@ class Flow(object):
         """
         # date_str = time.strftime("%Y-%m-%d", time.localtime())
         url_place = f"http://kjcx.nuaa.edu.cn/book/more/lib/{place}/type/4/day/{date}"
-        resp = requests.get(url_place, headers=self.headers, timeout=15)
-        resp.encoding = 'utf-8'
-        # print(resp.text)
-        soup = BeautifulSoup(resp.text, 'lxml')
-        # 提取预约链接
-        tag_url_booking = soup.select('a.btn.btn-info')[0]
-        url_booking = self.host + tag_url_booking['href']
-        return url_booking
+        retry_cnt = 4
+        while retry_cnt > 0:
+            resp = requests.get(url_place, headers=self.headers, timeout=10)
+            resp.encoding = 'utf-8'
+            # print(resp.text)
+            soup = BeautifulSoup(resp.text, 'lxml')
+            # 提取预约链接
+            tag_url_booking = soup.select('a.btn.btn-info')[0]
+            if tag_url_booking:
+                url_booking = self.host + tag_url_booking['href']
+                return url_booking
+            retry_cnt -= 1
+            time.sleep(random.uniform(0.4, 0.8))
+        return None
 
     def _get_flow_from_url(self, url):
         """
@@ -83,14 +91,42 @@ class Flow(object):
         # url = r"http://kjcx.nuaa.edu.cn/book/notice/act_id/650/type/4/lib/12"  # debug
         if not url:
             raise Exception('URL is None...')
-        resp = requests.get(url, headers=self.headers, timeout=15)  
-        resp.encoding = 'utf-8'
-        # print(resp.text)
-        re_people_num = re.compile(r'预约人数</b>：\[(\d+?)/(\d+?)\]')
-        people_info = re_people_num.search(resp.text)
-        people_info = [people_info.group(1), people_info.group(2)]
-        print(people_info)
-        return people_info
+        retry_cnt = 4
+        while retry_cnt > 0:
+            resp = requests.get(url, headers=self.headers, timeout=10)
+            resp.encoding = 'utf-8'
+            # print(resp.text)
+            re_people_num = re.compile(r'预约人数</b>：\[(\d+?)/(\d+?)\]')
+            people_info = re_people_num.search(resp.text)
+            if people_info:
+                people_info = [people_info.group(1), people_info.group(2)]
+                print(people_info)
+                return people_info
+            retry_cnt -= 1
+            time.sleep(random.uniform(0.4, 0.8))
+        return []
+
+    def _run_job(self, q, place, date):
+        try:
+            print(place)
+            place_id = self.lib_id[place]
+            url = self._get_booking_url(place_id, date)
+            info = self._get_flow_from_url(url)
+            now = info[0]
+            total = info[1]
+        except Exception as e:
+            print('\033[31m[ERROR]', e, '\033[0m')
+            now = ''
+            total = ''
+        data_single = dict(area=place, now=now, all=total)
+        q.put(data_single)
+        return data_single
+
+    def _read_job(self, q, rtn_list):
+        while True:
+            value = q.get(True)
+            print('Get from queue:', str(value))
+            rtn_list.append(value)
 
     def get_flow(self, date):
         """
@@ -104,20 +140,48 @@ class Flow(object):
             try:
                 url = self._get_booking_url(place_id, date)
                 info = self._get_flow_from_url(url)
-                current = info[0]
+                now = info[0]
                 total = info[1]
             except Exception as e:
                 print('\033[31m[ERROR]', e, '\033[0m')
-                current = ''
+                now = ''
                 total = ''
-            data_single = dict(area=place, current=current, total=total)
+            data_single = dict(area=place, now=now, all=total)
             data_all.append(data_single)
         return data_all
+
+    def get_flow_parallel(self, date):
+        """
+        Convert the data to a JSON list with multiprocessing.
+        """
+        cpu_count = multiprocessing.cpu_count()
+        print("cpu: ", cpu_count)
+        q = multiprocessing.Queue()
+        manager = multiprocessing.Manager()
+        return_list = manager.list()
+        pr_job = multiprocessing.Process(
+            target=self._read_job, args=(q, return_list))
+        # 启动子进程pr，读取:
+        pr_job.start()
+        pw_jobs = []
+        for place in self.lib_id:
+            p = multiprocessing.Process(
+                target=self._run_job, args=(q, place, date))
+            pw_jobs.append(p)
+            p.start()
+            time.sleep(random.uniform(0.05, 0.15))
+        for proc in pw_jobs:
+            proc.join()
+        # pr进程里是死循环，无法等待其结束，只能强行终止:
+        pr_job.terminate()
+        # self.data = list(return_list)
+        return list(return_list)
 
 
 if __name__ == "__main__":
     flow = Flow()
     time_start = time.time()
-    data = flow.get_flow(date='')
+    # data = flow.get_flow(date='')
+    data = flow.get_flow_parallel(date='')
     print(data)
     print(time.time()-time_start)
